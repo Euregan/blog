641dccabe1680304b35dcf177827a6878ed3f621
Valentin Manceaux-Panot <valentin@mcxp.io>
2023-12-09 17:37:06 +0100

    Added link to socials

--- src/Nav.css.ts
+++ src/Nav.css.ts
@@ -1,5 +1,41 @@
-import { style } from "@vanilla-extract/css";
+import { createVar, style } from "@vanilla-extract/css";
+import { calc } from "@vanilla-extract/css-utils";
 
-export const nav = style({ padding: 20 });
+export const nav = style({
+  padding: 20,
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+});
 
-export const item = style({ color: "white", textDecoration: "none" });
+export const link = style({
+  color: "white",
+  textDecoration: "none",
+  position: "relative",
+});
+
+const offset = createVar();
+
+export const linkContent = style({
+  lineHeight: 1,
+  vars: { [offset]: "-4px" },
+  borderBottom: "2px solid transparent",
+  selectors: {
+    [`${link}:hover &`]: {
+      marginTop: offset,
+      paddingBottom: calc.multiply(offset, -1),
+      borderBottom: "2px solid white",
+    },
+  },
+  transition: "all 0.2s cubic-bezier(.17,.84,.44,1)",
+});
+
+const baseCategory = style({
+  all: "unset",
+  listStyle: "none",
+  display: "flex",
+  gap: 10,
+});
+
+export const internals = style([baseCategory, {}]);
+
+export const externals = style([baseCategory, { justifyContent: "flex-end" }]);
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,13 +1,51 @@
 import { Link } from "wouter";
-import * as styles from "./Nav.css";
+import * as style from "./Nav.css";
+import { Github, Twitter, Linkedin } from "lucide-react";
 
 const Nav = () => (
-  <nav className={styles.nav}>
-    <Link href="/">
-      <a className={styles.item}>
-        <h1>Valentin Manceaux-Panot</h1>
-      </a>
-    </Link>
+  <nav className={style.nav}>
+    <ul className={style.internals}>
+      <li>
+        <Link href="/">
+          <a className={style.link}>
+            <h1 className={style.linkContent}>Valentin Manceaux-Panot</h1>
+          </a>
+        </Link>
+      </li>
+    </ul>
+
+    <ul className={style.externals}>
+      <li>
+        <a
+          target="_blank"
+          rel="noref noopener"
+          href="https://github.com/euregan/"
+          className={style.link}
+        >
+          <Github className={style.linkContent} />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noref noopener"
+          href="https://twitter.com/Euregan"
+          className={style.link}
+        >
+          <Twitter className={style.linkContent} />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noref noopener"
+          href="https://www.linkedin.com/in/valentin-manceaux-panot-0b160a67/"
+          className={style.link}
+        >
+          <Linkedin className={style.linkContent} />
+        </a>
+      </li>
+    </ul>
   </nav>
 );
 
